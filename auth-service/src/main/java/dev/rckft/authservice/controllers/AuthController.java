9a02b5c5c2f2719ecf8741e5c3446b7e9eae9e29
M S <Maciek>
2025-02-15 21:50:23 +0100

    added user model and user registration endpoint

--- auth-service/src/main/java/dev/rckft/authservice/controllers/AuthController.java
+++ auth-service/src/main/java/dev/rckft/authservice/controllers/AuthController.java
@@ -1,7 +1,10 @@
 package dev.rckft.authservice.controllers;
 
+import dev.rckft.authservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api")
 public class AuthController {
 
+    @Autowired
+    private UserService userService;
+
     @PostMapping("/register")
-    public ResponseEntity<?> register() throws Exception {
-        System.out.println("hello login");
+    public ResponseEntity<?> register(@RequestBody UserRegisterRequest request) {
+        userService.register(request);
         return ResponseEntity.ok().build();
     }
 }
