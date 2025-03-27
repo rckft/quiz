445656632a7bd069636074a121cce0cbab24c0b4
M S <Maciek>
2025-03-27 12:14:12 +0100

    add logging to various components

--- auth-service/src/main/java/dev/rckft/authservice/exception/ApiExceptionHandler.java
+++ auth-service/src/main/java/dev/rckft/authservice/exception/ApiExceptionHandler.java
@@ -1,5 +1,7 @@
 package dev.rckft.authservice.exception;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,6 +14,7 @@ import static org.springframework.http.HttpStatus.*;
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(ApiExceptionHandler.class);
     private final Clock clock;
 
     public ApiExceptionHandler(Clock clock) {
@@ -21,12 +24,14 @@ public class ApiExceptionHandler {
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleUserAlreadyExistsException(UserAlreadyExistsException exception) {
         HttpStatus status = BAD_REQUEST;
+        LOGGER.error(exception.getMessage());
         return ResponseEntity.status(status).body(getErrorResponse(status, exception));
     }
 
     @ExceptionHandler(InvalidTokenException.class)
     public ResponseEntity<ErrorResponse> handleInvalidTokenException(InvalidTokenException exception) {
         HttpStatus status = UNAUTHORIZED;
+        LOGGER.error(exception.getMessage());
         return ResponseEntity.status(status).body(getErrorResponse(status, exception));
     }
 
